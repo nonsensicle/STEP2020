945620f5ebe2989b3c7ca5b8b1be697a5cf603ce
Leah Ryu <ryu.leah@gmail.com>
2020-06-26 01:06:48 +0000

    - Add lightboxing to art gallery pages - Refine contact page - Change fonts and other formatting

---
+++ portfolio/src/main/webapp/lightbox.js
@@ -0,0 +1,52 @@
+/**
+ * Adding a lightbox feature to the art gallery pages (prints.html, illustrations.html, doodles.html) 
+ * Referred to https://www.w3schools.com/howto/howto_js_lightbox.asp
+ */
+
+// Display the box
+function openBox() {
+    document.getElementById("lightbox").style.display = "block";
+}
+
+// Close the box
+function closeBox() {
+    document.getElementById("lightbox").style.display = "none";
+}
+
+var whichImg = 0;
+showImg(whichImg);
+
+// Display a particular image in the lightbox
+function showImg(n) {
+    var imgs = document.getElementsByClassName("imgs");  // "imgs" is an array of html divs
+    var captionTxt = document.getElementById("caption");
+
+    // Wrap around if at beginning or end
+    if (n > imgs.length) {
+        whichImg = 0;
+    }
+    if (n < 0) {
+        whichImg = imgs.length - 1;
+    }
+  
+    // Hide all images
+    for (var i = 0; i < imgs.length; i++){
+        imgs[i].style.display = "none";   
+    }
+
+    // Display current image 
+    imgs[n].style.display = "block";
+
+    // Change caption to alt text by getting array of images inside the div "imgs"
+    captionTxt.innerHTML = imgs[n].getElementsByTagName('img')[0].alt;
+}
+
+// Next/previous (expected input 1 or -1)
+function slide(n) {
+    showImg(whichImg += n);
+}
+
+// Onclick for thumbnails; input: index
+function currentImg(n) {
+    showImg(whichImg = n);
+}
